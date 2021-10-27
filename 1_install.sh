9f1305d94c440eb82424cf009014c1e320857d1c
Gastón Martínez <gastongmartinez@gmail.com>
2021-10-26 22:56:10 -0300

    Remover paquete duplicado

--- 1_install.sh
+++ 1_install.sh
@@ -71,7 +71,6 @@ PAQUETES=(
     'gnome-extensions-app'
     'gnome-shell-extension-user-theme'
     'gnome-shell-extension-native-window-placement'
-    'gnome-shell-extension-user-theme'
     'gnome-shell-extension-dash-to-dock'
     'gnome-shell-extension-no-overview'
     'gnome-shell-extension-pop-shell'
