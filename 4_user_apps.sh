0eee711b0dd5fe9c06d7b345b2a377aa2e3e68fc
Gastón Martínez <gastongmartinez@gmail.com>
2021-11-05 04:12:56 -0300

    Flutter

--- 4_user_apps.sh
+++ 4_user_apps.sh
@@ -25,6 +25,16 @@ rm pycharm-community-2021.2.3.tar.gz
 rm ideaIC-2021.2.3.tar.gz
 cd ~ || return
 
+# Android
+cd ~/Apps || return
+wget https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2020.3.1.25/android-studio-2020.3.1.25-linux.tar.gz 
+tar -xzf android-studio-2020.3.1.25-linux.tar.gz
+rm android-studio-2020.3.1.25-linux.tar.gz
+wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_2.5.3-stable.tar.xz
+tar xf flutter_linux_2.5.3-stable.tar.xz
+rm flutter_linux_2.5.3-stable.tar.xz
+cd ~ || return
+
 # Bash
 git clone --recursive https://github.com/andresgongora/synth-shell.git
 chmod +x synth-shell/setup.sh
@@ -51,6 +61,8 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.local/share/
     echo 'setopt appendhistory'
     echo 'setopt sharehistory'
     echo 'setopt incappendhistory'
+    echo 'JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk'
+    echo 'export PATH="$HOME/anaconda3/bin:$HOME/Apps/flutter/bin:$HOME/.local/bin:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/bin:$HOME/.cargo/bin::$PATH"'
 } >>~/.zshrc
 chsh -s /usr/bin/zsh
 
