88f7581ada2017b48e8c6f9514b8a3bcd78c5052
Gastón Martínez <gastongmartinez@gmail.com>
2021-11-13 20:54:22 -0300

    Lazygit

--- 4_user_apps.sh
+++ 4_user_apps.sh
@@ -47,6 +47,13 @@ cd synth-shell || return
 cd ..
 rm -rf synth-shell
 
+# Lazygit
+git clone https://github.com/jesseduffield/lazygit.git
+cd lazygit || return
+go install
+cd ..
+rm -rf lazygit
+
 # ZSH
 if [ ! -d ~/.local/share/zsh ]; then
     mkdir ~/.local/share/zsh
@@ -66,7 +73,7 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.local/share/
     echo 'setopt sharehistory'
     echo 'setopt incappendhistory'
     echo 'JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk'
-    echo 'export PATH="$HOME/anaconda3/bin:$HOME/Apps/flutter/bin:$HOME/.local/bin:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/bin:$HOME/.cargo/bin:$PATH"'
+    echo 'export PATH="$HOME/anaconda3/bin:$HOME/Apps/flutter/bin:$HOME/.local/bin:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/bin:$HOME/.cargo/bin:$HOME/go/bin:$PATH"'
 } >>~/.zshrc
 chsh -s /usr/bin/zsh
 
