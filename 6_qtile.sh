392263662dac0d1818ef57e2b80574e45d391333
Gastón Martínez <gastongmartinez@gmail.com>
2022-02-02 04:13:06 -0300

    Instalacion Qtile

---
+++ 6_qtile.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Validacion del usuario ejecutando el script
+R_USER=$(id -u)
+if [ "$R_USER" -ne 0 ];
+then
+    echo -e "\nDebe ejecutar este script como root o utilizando sudo.\n"
+    exit 1
+fi
+
+dnf install python3-cffi -y
+dnf install python3-xcffib -y
+dnf install python3-cairocffi -y
+dnf install pango -y
+
+pip install --no-cache-dir cairocffi
+
+git clone git://github.com/qtile/qtile.git
+cd qtile || return
+pip install .
