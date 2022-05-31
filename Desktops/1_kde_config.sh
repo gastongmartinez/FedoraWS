f26949b4a505b2711ddee44e665b48709fa70667
Gastón Martínez <gastongmartinez@gmail.com>
2022-05-31 15:20:13 -0300

    Deshabilitar configuracion redundante en WS

--- Desktops/1_kde_config.sh
+++ Desktops/1_kde_config.sh
@@ -16,10 +16,10 @@ then
 fi
 
 ############################################################################################################################################
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip
-unzip JetBrainsMono.zip -d ~/.local/share/fonts
-fc-cache -f -v
-rm JetBrainsMono.zip
+# wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip
+# unzip JetBrainsMono.zip -d ~/.local/share/fonts
+# fc-cache -f -v
+# rm JetBrainsMono.zip
 
 ############################################# Tema WhiteSur #################################################################################
 git clone https://github.com/vinceliuice/WhiteSur-kde.git
@@ -27,36 +27,35 @@ cd WhiteSur-kde || return
 ./install.sh
 cd ..
 
-git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
-cd WhiteSur-gtk-theme || return
-./tweaks.sh -f
-cd ..
+# git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
+# cd WhiteSur-gtk-theme || return
+# ./tweaks.sh -f
+# cd ..
 
-git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git
-cd WhiteSur-icon-theme || return
-./install.sh -t grey
-cd ..
-
-git clone https://github.com/vinceliuice/WhiteSur-cursors.git
-cd WhiteSur-cursors || return
-./install.sh
-cd ..
+# git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git
+# cd WhiteSur-icon-theme || return
+# ./install.sh -t grey
+# cd ..
+# 
+# git clone https://github.com/vinceliuice/WhiteSur-cursors.git
+# cd WhiteSur-cursors || return
+# ./install.sh
+# cd ..
 
 rm -rf WhiteSur-kde
-rm -rf WhiteSur-icon-theme
-rm -rf WhiteSur-cursors
+# rm -rf WhiteSur-icon-theme
+# rm -rf WhiteSur-cursors
 #############################################################################################################################################
 
 # Autostart Apps
-if [ ! -d ~/.config/autostart ]; then
-    mkdir -p ~/.config/autostart
-fi
-# cp /usr/share/applications/plank.desktop ~/.config/autostart/
-cp /usr/share/applications/ulauncher.desktop ~/.config/autostart/
+# if [ ! -d ~/.config/autostart ]; then
+#     mkdir -p ~/.config/autostart
+# fi
+# cp /usr/share/applications/ulauncher.desktop ~/.config/autostart/
 
 # Tema Ulauncher
-mkdir -p ~/.config/ulauncher/user-themes
-git clone https://github.com/Raayib/WhiteSur-Dark-ulauncher.git ~/.config/ulauncher/user-themes/WhiteSur-Dark-ulauncher
+# mkdir -p ~/.config/ulauncher/user-themes
+# git clone https://github.com/Raayib/WhiteSur-Dark-ulauncher.git ~/.config/ulauncher/user-themes/WhiteSur-Dark-ulauncher
 
 
 ############################################## Config ##################################################################################
@@ -253,92 +252,92 @@ kwriteconfig5 --file "$HOME"/.config/powermanagementprofilesrc --group AC --grou
     echo 'inactiveForeground=170,170,170'
 } > "$HOME"/.config/kdeglobals
 
-{
-    echo '@define-color borders_breeze #686868;'
-    echo '@define-color content_view_bg_breeze #242424;'
-    echo '@define-color error_color_backdrop_breeze #da4453;'
-    echo '@define-color error_color_breeze #da4453;'
-    echo '@define-color error_color_insensitive_backdrop_breeze #5f2d32;'
-    echo '@define-color error_color_insensitive_breeze #5f2d32;'
-    echo '@define-color insensitive_base_color_breeze #222222;'
-    echo '@define-color insensitive_base_fg_color_breeze #6a6a6a;'
-    echo '@define-color insensitive_bg_color_breeze #333333;'
-    echo '@define-color insensitive_borders_breeze #444444;'
-    echo '@define-color insensitive_fg_color_breeze #767676;'
-    echo '@define-color insensitive_selected_bg_color_breeze #333333;'
-    echo '@define-color insensitive_selected_fg_color_breeze #767676;'
-    echo '@define-color insensitive_unfocused_bg_color_breeze #333333;'
-    echo '@define-color insensitive_unfocused_fg_color_breeze #767676;'
-    echo '@define-color insensitive_unfocused_selected_bg_color_breeze #333333;'
-    echo '@define-color insensitive_unfocused_selected_fg_color_breeze #767676;'
-    echo '@define-color link_color_breeze #315bef;'
-    echo '@define-color link_visited_color_breeze #787878;'
-    echo '@define-color success_color_backdrop_breeze #27ae60;'
-    echo '@define-color success_color_breeze #27ae60;'
-    echo '@define-color success_color_insensitive_backdrop_breeze #235036;'
-    echo '@define-color success_color_insensitive_breeze #235036;'
-    echo '@define-color theme_base_color_breeze #242424;'
-    echo '@define-color theme_bg_color_breeze #363636;'
-    echo '@define-color theme_button_background_backdrop_breeze #656565;'
-    echo '@define-color theme_button_background_backdrop_insensitive_breeze #606060;'
-    echo '@define-color theme_button_background_insensitive_breeze #606060;'
-    echo '@define-color theme_button_background_normal_breeze #656565;'
-    echo '@define-color theme_button_decoration_focus_backdrop_breeze #315bef;'
-    echo '@define-color theme_button_decoration_focus_backdrop_insensitive_breeze #4f5d8e;'
-    echo '@define-color theme_button_decoration_focus_breeze #315bef;'
-    echo '@define-color theme_button_decoration_focus_insensitive_breeze #4f5d8e;'
-    echo '@define-color theme_button_decoration_hover_backdrop_breeze #315bef;'
-    echo '@define-color theme_button_decoration_hover_backdrop_insensitive_breeze #4f5d8e;'
-    echo '@define-color theme_button_decoration_hover_breeze #315bef;'
-    echo '@define-color theme_button_decoration_hover_insensitive_breeze #4f5d8e;'
-    echo '@define-color theme_button_foreground_active_backdrop_breeze #fcfcfc;'
-    echo '@define-color theme_button_foreground_active_backdrop_insensitive_breeze #767676;'
-    echo '@define-color theme_button_foreground_active_breeze #ffffff;'
-    echo '@define-color theme_button_foreground_active_insensitive_breeze #767676;'
-    echo '@define-color theme_button_foreground_backdrop_breeze #fcfcfc;'
-    echo '@define-color theme_button_foreground_backdrop_insensitive_breeze #939393;'
-    echo '@define-color theme_button_foreground_insensitive_breeze #939393;'
-    echo '@define-color theme_button_foreground_normal_breeze #fcfcfc;'
-    echo '@define-color theme_fg_color_breeze #fcfcfc;'
-    echo '@define-color theme_hovering_selected_bg_color_breeze #ffffff;'
-    echo '@define-color theme_selected_bg_color_breeze #315bef;'
-    echo '@define-color theme_selected_fg_color_breeze #ffffff;'
-    echo '@define-color theme_text_color_breeze #fcfcfc;'
-    echo '@define-color theme_titlebar_background_backdrop_breeze #424242;'
-    echo '@define-color theme_titlebar_background_breeze #333333;'
-    echo '@define-color theme_titlebar_background_light_breeze #363636;'
-    echo '@define-color theme_titlebar_foreground_backdrop_breeze #aaaaaa;'
-    echo '@define-color theme_titlebar_foreground_breeze #fcfcfc;'
-    echo '@define-color theme_titlebar_foreground_insensitive_backdrop_breeze #aaaaaa;'
-    echo '@define-color theme_titlebar_foreground_insensitive_breeze #aaaaaa;'
-    echo '@define-color theme_unfocused_base_color_breeze #242424;'
-    echo '@define-color theme_unfocused_bg_color_breeze #363636;'
-    echo '@define-color theme_unfocused_fg_color_breeze #fcfcfc;'
-    echo '@define-color theme_unfocused_selected_bg_color_alt_breeze #263c88;'
-    echo '@define-color theme_unfocused_selected_bg_color_breeze #263c88;'
-    echo '@define-color theme_unfocused_selected_fg_color_breeze #fcfcfc;'
-    echo '@define-color theme_unfocused_text_color_breeze #fcfcfc;'
-    echo '@define-color theme_unfocused_view_bg_color_breeze #222222;'
-    echo '@define-color theme_unfocused_view_text_color_breeze #6a6a6a;'
-    echo '@define-color theme_view_active_decoration_color_breeze #315bef;'
-    echo '@define-color theme_view_hover_decoration_color_breeze #315bef;'
-    echo '@define-color tooltip_background_breeze #333333;'
-    echo '@define-color tooltip_border_breeze #656565;'
-    echo '@define-color tooltip_text_breeze #fcfcfc;'
-    echo '@define-color unfocused_borders_breeze #686868;'
-    echo '@define-color unfocused_insensitive_borders_breeze #444444;'
-    echo '@define-color warning_color_backdrop_breeze #f67400;'
-    echo '@define-color warning_color_breeze #f67400;'
-    echo '@define-color warning_color_insensitive_backdrop_breeze #683d16;'
-    echo '@define-color warning_color_insensitive_breeze #683d16;'
-} > "$HOME"/.config/gtk-3.0/colors.css
+# {
+#     echo '@define-color borders_breeze #686868;'
+#     echo '@define-color content_view_bg_breeze #242424;'
+#     echo '@define-color error_color_backdrop_breeze #da4453;'
+#     echo '@define-color error_color_breeze #da4453;'
+#     echo '@define-color error_color_insensitive_backdrop_breeze #5f2d32;'
+#     echo '@define-color error_color_insensitive_breeze #5f2d32;'
+#     echo '@define-color insensitive_base_color_breeze #222222;'
+#     echo '@define-color insensitive_base_fg_color_breeze #6a6a6a;'
+#     echo '@define-color insensitive_bg_color_breeze #333333;'
+#     echo '@define-color insensitive_borders_breeze #444444;'
+#     echo '@define-color insensitive_fg_color_breeze #767676;'
+#     echo '@define-color insensitive_selected_bg_color_breeze #333333;'
+#     echo '@define-color insensitive_selected_fg_color_breeze #767676;'
+#     echo '@define-color insensitive_unfocused_bg_color_breeze #333333;'
+#     echo '@define-color insensitive_unfocused_fg_color_breeze #767676;'
+#     echo '@define-color insensitive_unfocused_selected_bg_color_breeze #333333;'
+#     echo '@define-color insensitive_unfocused_selected_fg_color_breeze #767676;'
+#     echo '@define-color link_color_breeze #315bef;'
+#     echo '@define-color link_visited_color_breeze #787878;'
+#     echo '@define-color success_color_backdrop_breeze #27ae60;'
+#     echo '@define-color success_color_breeze #27ae60;'
+#     echo '@define-color success_color_insensitive_backdrop_breeze #235036;'
+#     echo '@define-color success_color_insensitive_breeze #235036;'
+#     echo '@define-color theme_base_color_breeze #242424;'
+#     echo '@define-color theme_bg_color_breeze #363636;'
+#     echo '@define-color theme_button_background_backdrop_breeze #656565;'
+#     echo '@define-color theme_button_background_backdrop_insensitive_breeze #606060;'
+#     echo '@define-color theme_button_background_insensitive_breeze #606060;'
+#     echo '@define-color theme_button_background_normal_breeze #656565;'
+#     echo '@define-color theme_button_decoration_focus_backdrop_breeze #315bef;'
+#     echo '@define-color theme_button_decoration_focus_backdrop_insensitive_breeze #4f5d8e;'
+#     echo '@define-color theme_button_decoration_focus_breeze #315bef;'
+#     echo '@define-color theme_button_decoration_focus_insensitive_breeze #4f5d8e;'
+#     echo '@define-color theme_button_decoration_hover_backdrop_breeze #315bef;'
+#     echo '@define-color theme_button_decoration_hover_backdrop_insensitive_breeze #4f5d8e;'
+#     echo '@define-color theme_button_decoration_hover_breeze #315bef;'
+#     echo '@define-color theme_button_decoration_hover_insensitive_breeze #4f5d8e;'
+#     echo '@define-color theme_button_foreground_active_backdrop_breeze #fcfcfc;'
+#     echo '@define-color theme_button_foreground_active_backdrop_insensitive_breeze #767676;'
+#     echo '@define-color theme_button_foreground_active_breeze #ffffff;'
+#     echo '@define-color theme_button_foreground_active_insensitive_breeze #767676;'
+#     echo '@define-color theme_button_foreground_backdrop_breeze #fcfcfc;'
+#     echo '@define-color theme_button_foreground_backdrop_insensitive_breeze #939393;'
+#     echo '@define-color theme_button_foreground_insensitive_breeze #939393;'
+#     echo '@define-color theme_button_foreground_normal_breeze #fcfcfc;'
+#     echo '@define-color theme_fg_color_breeze #fcfcfc;'
+#     echo '@define-color theme_hovering_selected_bg_color_breeze #ffffff;'
+#     echo '@define-color theme_selected_bg_color_breeze #315bef;'
+#     echo '@define-color theme_selected_fg_color_breeze #ffffff;'
+#     echo '@define-color theme_text_color_breeze #fcfcfc;'
+#     echo '@define-color theme_titlebar_background_backdrop_breeze #424242;'
+#     echo '@define-color theme_titlebar_background_breeze #333333;'
+#     echo '@define-color theme_titlebar_background_light_breeze #363636;'
+#     echo '@define-color theme_titlebar_foreground_backdrop_breeze #aaaaaa;'
+#     echo '@define-color theme_titlebar_foreground_breeze #fcfcfc;'
+#     echo '@define-color theme_titlebar_foreground_insensitive_backdrop_breeze #aaaaaa;'
+#     echo '@define-color theme_titlebar_foreground_insensitive_breeze #aaaaaa;'
+#     echo '@define-color theme_unfocused_base_color_breeze #242424;'
+#     echo '@define-color theme_unfocused_bg_color_breeze #363636;'
+#     echo '@define-color theme_unfocused_fg_color_breeze #fcfcfc;'
+#     echo '@define-color theme_unfocused_selected_bg_color_alt_breeze #263c88;'
+#     echo '@define-color theme_unfocused_selected_bg_color_breeze #263c88;'
+#     echo '@define-color theme_unfocused_selected_fg_color_breeze #fcfcfc;'
+#     echo '@define-color theme_unfocused_text_color_breeze #fcfcfc;'
+#     echo '@define-color theme_unfocused_view_bg_color_breeze #222222;'
+#     echo '@define-color theme_unfocused_view_text_color_breeze #6a6a6a;'
+#     echo '@define-color theme_view_active_decoration_color_breeze #315bef;'
+#     echo '@define-color theme_view_hover_decoration_color_breeze #315bef;'
+#     echo '@define-color tooltip_background_breeze #333333;'
+#     echo '@define-color tooltip_border_breeze #656565;'
+#     echo '@define-color tooltip_text_breeze #fcfcfc;'
+#     echo '@define-color unfocused_borders_breeze #686868;'
+#     echo '@define-color unfocused_insensitive_borders_breeze #444444;'
+#     echo '@define-color warning_color_backdrop_breeze #f67400;'
+#     echo '@define-color warning_color_breeze #f67400;'
+#     echo '@define-color warning_color_insensitive_backdrop_breeze #683d16;'
+#     echo '@define-color warning_color_insensitive_breeze #683d16;'
+# } > "$HOME"/.config/gtk-3.0/colors.css
 
 sed -i "s/gtk-icon-theme-name=breeze-dark/gtk-icon-theme-name=WhiteSur-grey-dark/g" "$HOME/.config/gtk-3.0/settings.ini"
 sed -i "s/gtk-font-name=Noto Sans,  10/gtk-font-name=Noto Sans,  11/g" "$HOME/.config/gtk-3.0/settings.ini"
 sed -i "s/gtk-icon-theme-name=breeze-dark/gtk-icon-theme-name=WhiteSur-grey-dark/g" "$HOME/.config/gtk-4.0/settings.ini"
 sed -i "s/gtk-font-name=Noto Sans,  10/gtk-font-name=Noto Sans,  11/g" "$HOME/.config/gtk-4.0/settings.ini"
 sed -i "s/org.kde.breezedark.desktop/com.github.vinceliuice.WhiteSur-dark/g" "$HOME/.config/kdedefaults/package"
-sed -i "s/\"theme-name\": \"light\"/\"theme-name\": \"WhiteSur-Dark\"/g" "$HOME/.config/ulauncher/settings.json"
+# sed -i "s/\"theme-name\": \"light\"/\"theme-name\": \"WhiteSur-Dark\"/g" "$HOME/.config/ulauncher/settings.json"
 sed -i "s/Net\/IconThemeName \"breeze-dark\"/Net\/IconThemeName \"WhiteSur-grey-dark\"/g" "$HOME/.config/xsettingsd/xsettingsd.conf"
 sed -i "s/Gtk\/FontName \"Noto Sans,  10\"/Gtk\/FontName \"Noto Sans,  11\"/g" "$HOME/.config/xsettingsd/xsettingsd.conf"
 #############################################################################################################################################
