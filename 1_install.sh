12159cc3979770064297864e8b68c4ee031c7be4
Gastón Martínez <gastongmartinez@gmail.com>
2023-03-01 20:09:53 -0300

    Librewolf

--- 1_install.sh
+++ 1_install.sh
@@ -71,6 +71,9 @@ dnf makecache
 sh -c 'echo -e "[brave-browser-rpm-release.s3.brave.com_x86_64_]\nname=created by dnf config-manager from https://brave-browser-rpm-release.s3.brave.com/x86_64/\nbaseurl=https://brave-browser-rpm-release.s3.brave.com/x86_64/\nenabled=1" > /etc/yum.repos.d/brave-browser-rpm-release.s3.brave.com_x86_64_.repo'
 rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 
+# Librewolf
+dnf config-manager --add-repo https://rpm.librewolf.net/librewolf-repo.repo
+
 # CORP
 dnf copr enable frostyx/qtile -y
 dnf copr enable atim/lazygit -y
@@ -102,6 +105,7 @@ PAQUETES=(
 
     #### WEB ####
     'chromium'
+    'librewolf'
     'thunderbird'
     'remmina'
     'qbittorrent'
