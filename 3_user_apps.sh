e3b8557d2086a60a0c30712358c785fb6959a6e3
Gastón Martínez <gastongmartinez@gmail.com>
2023-03-27 09:28:12 -0300

    Limpieza y LSPs nvim-helix

--- 3_user_apps.sh
+++ 3_user_apps.sh
@@ -5,15 +5,13 @@ flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flath
 flatpak --user install flathub fr.handbrake.ghb -y
 flatpak --user install flathub md.obsidian.Obsidian -y
 flatpak --user install flathub com.mattjakeman.ExtensionManager -y
-# flatpak --user install flathub com.jetbrains.PyCharm-Community -y
-# flatpak --user install flathub com.jetbrains.IntelliJ-IDEA-Community -y
-# flatpak --user install flathub com.google.AndroidStudio -y
 flatpak --user install flathub io.github.shiftey.Desktop -y
 flatpak --user install flathub net.ankiweb.Anki -y
 flatpak --user install flathub com.github.marktext.marktext -y
 flatpak --user install flathub com.rafaelmardojai.Blanket -y
 flatpak --user install flathub com.github.tchx84.Flatseal -y
 flatpak --user install flathub com.github.neithern.g4music -y
+flatpak --user install flathub com.axosoft.GitKraken -y
 
 # Doom Emacs
 if [ -d ~/.emacs.d ]; then
@@ -30,52 +28,48 @@ git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
 sleep 5
 rm -rf ~/.doom.d
 sudo npm -g install stylelint js-beautify
-sudo cp /usr/share/applications/emacs.desktop /usr/share/applications/emacsClient.desktop
-sudo sed -i "s/Name=Emacs/Name=Emacs Client/g" "/usr/share/applications/emacsClient.desktop"
-sudo sed -i "s/Exec=emacs/Exec=emacsclient -c -a 'emacs'/g" "/usr/share/applications/emacsClient.desktop" 
-
-# NeoVim
-# mkdir -p ~/.config/nvim
-# git clone https://github.com/gastongmartinez/Nvim ~/.config/nvim
+# sudo cp /usr/share/applications/emacs.desktop /usr/share/applications/emacsClient.desktop
+# sudo sed -i "s/Name=Emacs/Name=Emacs Client/g" "/usr/share/applications/emacsClient.desktop"
+# sudo sed -i "s/Exec=emacs/Exec=emacsclient -c -a 'emacs'/g" "/usr/share/applications/emacsClient.desktop" 
 
 # Tmux Plugin Manager
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 
 # Anaconda
-wget https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh
-chmod +x Anaconda3-2022.10-Linux-x86_64.sh
-./Anaconda3-2022.10-Linux-x86_64.sh
+# wget https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh
+# chmod +x Anaconda3-2022.10-Linux-x86_64.sh
+# ./Anaconda3-2022.10-Linux-x86_64.sh
 
 # Jetbrains
 if [ ! -d ~/Apps ]; then
     mkdir ~/Apps
 fi
-cd ~/Apps || return
-wget https://download.jetbrains.com/python/pycharm-professional-2022.3.tar.gz
-wget https://download.jetbrains.com/datagrip/datagrip-2022.3.1.tar.gz
-wget https://download.jetbrains.com/cpp/CLion-2022.3.tar.gz
-tar -xzf pycharm-professional-2022.3.tar.gz
-tar -xzf datagrip-2022.3.1.tar.gz
-tar -xzf CLion-2022.3.tar.gz
-rm pycharm-professional-2022.3.tar.gz
-rm datagrip-2022.3.1.tar.gz
-rm CLion-2022.3.tar.gz
-cd ~ || return
+# cd ~/Apps || return
+# wget https://download.jetbrains.com/python/pycharm-professional-2022.3.tar.gz
+# wget https://download.jetbrains.com/datagrip/datagrip-2022.3.1.tar.gz
+# wget https://download.jetbrains.com/cpp/CLion-2022.3.tar.gz
+# tar -xzf pycharm-professional-2022.3.tar.gz
+# tar -xzf datagrip-2022.3.1.tar.gz
+# tar -xzf CLion-2022.3.tar.gz
+# rm pycharm-professional-2022.3.tar.gz
+# rm datagrip-2022.3.1.tar.gz
+# rm CLion-2022.3.tar.gz
+# cd ~ || return
 
 
 # Android
-if [ ! -d ~/Apps ]; then
-    mkdir ~/Apps
-fi
-cd ~/Apps || return
-wget https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2021.3.1.17/android-studio-2021.3.1.17-linux.tar.gz
-tar -xzf android-studio-2021.3.1.17-linux.tar.gz
-rm android-studio-2021.3.1.17-linux.tar.gz
-cd ~/Apps || return
-wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.3.9-stable.tar.xz
-tar xf flutter_linux_3.3.9-stable.tar.xz
-rm flutter_linux_3.3.9-stable.tar.xz
-cd ~ || return
+# if [ ! -d ~/Apps ]; then
+#     mkdir ~/Apps
+# fi
+# cd ~/Apps || return
+# wget https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2021.3.1.17/android-studio-2021.3.1.17-linux.tar.gz
+# tar -xzf android-studio-2021.3.1.17-linux.tar.gz
+# rm android-studio-2021.3.1.17-linux.tar.gz
+# cd ~/Apps || return
+# wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.3.9-stable.tar.xz
+# tar xf flutter_linux_3.3.9-stable.tar.xz
+# rm flutter_linux_3.3.9-stable.tar.xz
+# cd ~ || return
 
 # Bash
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
@@ -85,7 +79,6 @@ sed -i 's/"font"/"powerline"/g' "$HOME/.bashrc"
 if [ ! -d ~/.config/autostart ]; then
     mkdir -p ~/.config/autostart
 fi
-# cp /usr/share/applications/plank.desktop ~/.config/autostart/
 cp /usr/share/applications/ulauncher.desktop ~/.config/autostart/
 
 # ZSH
@@ -111,8 +104,12 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.local/share/
 } >>~/.zshrc
 chsh -s /usr/bin/zsh
 
+pip install black 'python-lsp-server[all]' pyright yamllint autopep8
 cargo install taplo-cli --locked
-sudo npm install -g dockerfile-language-server-nodejs
+cargo install stylua
+sudo npm install -g neovim prettier bash-language-server vscode-langservers-extracted emmet-ls typescript typescript-language-server yaml-language-server live-server markdownlint markdownlint-cli dockerfile-language-server-nodejs stylelint js-beautify
+
+jgmenu_run init --theme=archlabs_1803
 
 sleep 5
 
