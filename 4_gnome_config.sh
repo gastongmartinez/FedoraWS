62c463cd86cbaba094f5efed17a3f2d3b73d723d
Gastón Martínez <gastongmartinez@gmail.com>
2023-03-27 09:29:07 -0300

    Configuracion jgmenu

--- 4_gnome_config.sh
+++ 4_gnome_config.sh
@@ -139,6 +139,11 @@ flatpak --user override --filesystem="$HOME"/.themes
 flatpak --user override --env=GTK_THEME=WhiteSur-Dark
 #############################################################################################################################################
 
+{
+    echo 'menu_margin_x        = 10'
+    echo 'menu_margin_y        = 480'
+} >>~/.config/jgmenu/jgmenurc
+
 sleep 5
 
 reboot
