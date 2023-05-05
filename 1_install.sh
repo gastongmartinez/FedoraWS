614a85dcb627242a31beab0270f3fb8d6ff89abb
Gastón Martínez <gastongmartinez@gmail.com>
2023-05-05 18:54:50 -0300

    Agrega calibre y habilita cambio mesa

--- 1_install.sh
+++ 1_install.sh
@@ -43,12 +43,12 @@ dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(
 dnf install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm -y
 
 # MESA
-# read -rp "Cambiar drivers de video a MESA Freeworld? (S/N): " MESA
-# if [ "$MESA" == 'S' ]; 
-# then
-#     dnf swap mesa-va-drivers mesa-va-drivers-freeworld -y
-#     dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld -y
-# fi
+read -rp "Cambiar drivers de video a MESA Freeworld? (S/N): " MESA
+if [ "$MESA" == 'S' ]; 
+then
+    dnf swap mesa-va-drivers mesa-va-drivers-freeworld -y
+    dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld -y
+fi
 
 # Repositorios VSCode y Powershell 
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
@@ -146,6 +146,7 @@ PAQUETES=(
     'the_silver_searcher'
     'qalculate'
     'qalculate-gtk'
+    'calibre'
     'foliate'
     'aspell'
     'pandoc'
